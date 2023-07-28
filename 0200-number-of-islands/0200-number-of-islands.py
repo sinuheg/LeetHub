3ff4d09314584d098dea44dd392ce8e84831ba0e
Sinuhe Guerra <sinuhe.guerra@gmail.com>
2023-07-27 21:49:43 -0700

    Making 002 executable.

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,4 +1,11 @@
 """
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
+return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally 
+or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Pseudo code:
 iterate matrix row col
 if find a 1
     add island count
@@ -15,6 +22,8 @@ flip the island
     check left
     check right
 """
+
+from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
@@ -39,3 +48,28 @@ class Solution:
         self.flip_island(grid, row, col-1)
         self.flip_island(grid, row, col+1)
         
+sol = Solution()
+# Test 1:
+# Input
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+# Expected Output: 1
+print(sol.numIslands(grid))
+
+
+
+# Test 2:
+
+# Input: 
+grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+# Expected Output: 3
+print(sol.numIslands(grid))
