b43f17595bdf2264644bf0c4d37c52213db0f088
Sinuhe Guerra <sinuhe.guerra@gmail.com>
2023-08-05 18:44:39 -0700

    Adding brackets challenge.

---
+++ speedtyper/brackets.py
@@ -0,0 +1,6 @@
+def print_brackets():
+    print("{j {j {j {j {j {j {j {j {j ")
+    print("}j }j }j }j }j }j }j }j }j ")
+    print("]j ]j ]j ]j ]j ]j ]j ]j ]j ")
+    print("{f}{f}{f}{f}{j}j{j}j{j}j{j}")
+    
