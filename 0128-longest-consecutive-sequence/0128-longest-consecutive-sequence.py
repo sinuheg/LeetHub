afb719b83b8375a3842b257f1e7cb939e088dda7
Sinuhe Guerra <sinuhe.guerra@gmail.com>
2023-09-22 15:02:25 -0700

    Time: 377 ms (63.05%), Space: 37 MB (8.1%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,29 +1,3 @@
-'''
-[100,4,200,1,3,2]
-               ^
-{4, 3 , 2, 1}
-max 4
-
-
-1 2 3 4 78 98 54 21 7 8 9 10 11
-                              ^
-{1, 2, 3, 4, 7, 8, 9 ,10, 11}
-curr = 5
-max = 4
-
-find window
-    finds upward and downwards in original set
-    if found adds to used numbers
-    count increases for each number added
-    updates max
-
-for each element
-    if not in used
-        find window
-    if used
-        go to next
-'''
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         originals = set(nums)
